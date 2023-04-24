ec5ccfa34f7e7ad386eaa3ba9b9aa8223ed02423
genfengl <genfeng.liu@gmail.com>
2023-04-24 23:23:28 +1000

    union types

--- union-types/app.ts
+++ union-types/app.ts
@@ -1,7 +1,12 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text'
+
+
 function combine(
-    input1: number | string,
+    // type alias can be re-used
+    input1: Combinable,
     input2: number | string,
-    resultConversion: 'as-number' | 'as-text'
+    resultConversion: ConversionDescriptor
 ) {
     let result
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
