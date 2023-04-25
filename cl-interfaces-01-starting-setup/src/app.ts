0be0d3f7e871ce030734bf4464ddf953d11856d0
genfengl <genfeng.liu@gmail.com>
2023-04-25 19:21:54 +1000

    interfaces

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,138 +1,51 @@
-// * abstract is used to enforce that all classes based on a certain class share some same abstract methods or properties
-//* abstract class cannot be instantiated, but only inherited
-abstract class Department {
-    //* static cannot be accessed using 'this'
-    //* has to use Department.fiscalYear instead
-    static fiscalYear = 2020
+//* interface only in TS
 
-    // private id: string
-    // private name: string;
-    //* private means employees can only be accessed inside the class
-    //* so that only the methods in the class can access it
-    //* protected means can be accessed inside the class and any extended/inherited classes
-    protected employees: string[] = []
+// type AddFn = (a: number, b: number) => number
+// function interface
+interface AddFn {
+    (a: number, b: number): number
+}
 
-    constructor(protected readonly id: string, public name: string) {
-        this.id = id
-        this.name = name
+let add: AddFn
+
+add = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
+interface Named {
+    readonly name?: string
+    //* ? means optional
+    outputName?: string
+}
+
+// * can use readonly in interface
+//* can extend/inherit from multiple interfaces
+interface Greetable extends Named {
+    greet(phrase: string): void
+}
+
+//* you can implement multiple interfaces, separated by ,
+class Person implements Greetable {
+    name?: string
+    age = 30
+
+    constructor(n?: string) {
+        if (n) {
+            this.name = n
+        }
     }
 
-    static createEmployee(name: string) {
-        return { name: name }
-    }
-
-    // defining the type for the argument to be Department class
-    abstract describe(this: Department): void
-
-    addEmployee(employee: string) {
-        this.employees.push(employee)
-    }
-
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+    greet(phrase: string) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name)
+        }
     }
 }
 
-//  can only inherit from one class
-//  have to add super when adding constructor in class that are inherited
-class ITDepartment extends Department {
-    constructor(id: string, public admins: string[]) {
-        // call super first before using 'this'
-        super(id, 'IT')
-        this.admins = admins
-    }
+let user1: Greetable
 
-    describe(): void {
-        console.log('IT Department - ID: ' + this.id);
-    }
-}
+user1 = new Person('Max')
 
-class AccountingDepartment extends Department {
-    private lastReport: string
-    private static instance: AccountingDepartment
+console.log('Hi there - I am ');
+console.log(user1);
 
-    static getInstance() {
-        if (AccountingDepartment.instance) {
-            return this.instance
-        }
-        this.instance = new AccountingDepartment('d2', [])
-        return this.instance
-    }
-
-    // setter method
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error('Please pass in a valid value!')
-        }
-        this.addReport(value)
-    }
-
-    // getter method: has to return sth
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
-        }
-        throw new Error('No report found.')
-    }
-
-    // use private constructor for singletons, to enforce that a class is only created once
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting')
-        this.lastReport = reports[0]
-    }
-
-    describe() {
-        console.log('Accounting Department - ID: ' + this.id)
-    }
-
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            return
-        }
-        this.employees.push(name)
-    }
-
-    addReport(text: string) {
-        this.reports.push(text)
-        this.lastReport = text
-    }
-
-    printReports() {
-        console.log(this.reports);
-    }
-}
-
-const employee1 = Department.createEmployee('Max')
-console.log(employee1, Department.fiscalYear);
-
-
-const it = new ITDepartment('d1', ['Max'])
-
-it.addEmployee('Max')
-it.addEmployee('Manu')
-it.describe()
-
-// accounting.employees[2] = 'Anna'
-
-
-it.printEmployeeInformation()
-
-console.log(it);
-
-// const accounting = new AccountingDepartment('d2', [])
-const accounting = AccountingDepartment.getInstance()
-
-// accessing the setter method
-accounting.mostRecentReport = 'Year End Report'
-accounting.addEmployee('Max')
-accounting.addReport('Something went wrong...')
-accounting.printReports()
-console.log(accounting.mostRecentReport);
-console.log(accounting);
-accounting.describe()
-
-
-// const accountingCopy = { name: 's', describe: accounting.describe }
-
-// accountingCopy.describe()
