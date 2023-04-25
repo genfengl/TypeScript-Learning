30942adc23e0a5b21815d197366464c4e09ba5b9
genfengl <genfeng.liu@gmail.com>
2023-04-25 23:55:10 +1000

    generic types

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,1 +1,42 @@
-// Code goes here!
+const names: Array<string> = [] // string[]
+// names[0].split(' ')
+
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!')
+//     }, 2000)
+// })
+
+// promise.then(data => {
+//     data.split(' ')
+// })
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB)
+}
+
+// * the type of object T and object U are inferred here
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj);
+
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe(element: string) {
+  let descriptionText = 'Got no value.'
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.'
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.'
+  }
+  return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there!'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return obj[key]
+}
+
+console.log(extractAndConvert({ name: 'Max' }, 'name'));
