e269c56a4e8ef8b93ca665d451fc321d1a81978b
genfengl <genfeng.liu@gmail.com>
2023-04-25 18:16:40 +1000

    classes

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,18 +1,28 @@
-class Department {
+// * abstract is used to enforce that all classes based on a certain class share some same abstract methods or properties
+//* abstract class cannot be instantiated, but only inherited
+abstract class Department {
+    //* static cannot be accessed using 'this'
+    //* has to use Department.fiscalYear instead
+    static fiscalYear = 2020
+
     // private id: string
     // private name: string;
     //* private means employees can only be accessed inside the class
     //* so that only the methods in the class can access it
-    private employees: string[] = []
+    //* protected means can be accessed inside the class and any extended/inherited classes
+    protected employees: string[] = []
 
-    constructor(private readonly id: string, public name: string) {
+    constructor(protected readonly id: string, public name: string) {
         this.id = id
         this.name = name
     }
 
-    describe(this: Department) {
-        console.log(`Deparment (${this.id}): ${this.name}`);
+    static createEmployee(name: string) {
+        return { name: name }
     }
+
+    // defining the type for the argument to be Department class
+    abstract describe(this: Department): void
 
     addEmployee(employee: string) {
         this.employees.push(employee)
@@ -27,44 +37,100 @@ class Department {
 //  can only inherit from one class
 //  have to add super when adding constructor in class that are inherited
 class ITDepartment extends Department {
-     constructor(id: string, public admins: string[]) {
+    constructor(id: string, public admins: string[]) {
         // call super first before using 'this'
         super(id, 'IT')
         this.admins = admins
-     }
+    }
+
+    describe(): void {
+        console.log('IT Department - ID: ' + this.id);
+    }
 }
 
 class AccountingDepartment extends Department {
-    constructor(id: string, private reports: string[]) {
+    private lastReport: string
+    private static instance: AccountingDepartment
+
+    static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance
+        }
+        this.instance = new AccountingDepartment('d2', [])
+        return this.instance
+    }
+
+    // setter method
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!')
+        }
+        this.addReport(value)
+    }
+
+    // getter method: has to return sth
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport
+        }
+        throw new Error('No report found.')
+    }
+
+    // use private constructor for singletons, to enforce that a class is only created once
+    private constructor(id: string, private reports: string[]) {
         super(id, 'Accounting')
+        this.lastReport = reports[0]
+    }
+
+    describe() {
+        console.log('Accounting Department - ID: ' + this.id)
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return
+        }
+        this.employees.push(name)
     }
 
     addReport(text: string) {
         this.reports.push(text)
+        this.lastReport = text
     }
 
     printReports() {
-        console.log(this.reports);        
+        console.log(this.reports);
     }
 }
+
+const employee1 = Department.createEmployee('Max')
+console.log(employee1, Department.fiscalYear);
+
 
 const it = new ITDepartment('d1', ['Max'])
 
 it.addEmployee('Max')
 it.addEmployee('Manu')
+it.describe()
 
 // accounting.employees[2] = 'Anna'
 
-it.describe()
+
 it.printEmployeeInformation()
 
 console.log(it);
 
+// const accounting = new AccountingDepartment('d2', [])
+const accounting = AccountingDepartment.getInstance()
 
-const accounting = new AccountingDepartment('d2', [])
-
+// accessing the setter method
+accounting.mostRecentReport = 'Year End Report'
+accounting.addEmployee('Max')
 accounting.addReport('Something went wrong...')
 accounting.printReports()
+console.log(accounting.mostRecentReport);
+console.log(accounting);
+accounting.describe()
 
 
 // const accountingCopy = { name: 's', describe: accounting.describe }
