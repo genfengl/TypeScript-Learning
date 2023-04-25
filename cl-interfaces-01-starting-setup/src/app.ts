9ba0cefd0141f1bccd73fde0a6c5619d4da1891f
genfengl <genfeng.liu@gmail.com>
2023-04-25 16:59:33 +1000

    private for class

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,11 +1,37 @@
 class Department {
     name: string;
+    //* private means employees can only be accessed inside the class
+    //* so that only the methods in the class can access it
+    private employees: string[] = []
 
     constructor(n: string) {
         this.name = n
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name);
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 
 const accounting = new Department('Accounting')
 
-console.log(accounting);
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+accounting.employees[2] = 'Anna'
+
+accounting.describe()
+accounting.printEmployeeInformation()
+
+// const accountingCopy = { name: 's', describe: accounting.describe }
+
+// accountingCopy.describe()
