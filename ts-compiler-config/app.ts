36d00ffce0dc16a2ff633120e52fcad8216d59f0
genfengl <genfeng.liu@gmail.com>
2023-04-25 14:40:41 +1000

    learnt --watch and --init

--- ts-compiler-config/app.ts
+++ ts-compiler-config/app.ts
@@ -1,5 +1,10 @@
+// use --watch for watch mode
+//  use tsc --init, and then tsc to compile the whole project dir, just like node
+//  can be combined with --watch to enter watch mode for the whole project
+
+
 const userName = 'Max'
-userName = 3
+
 
 console.log(userName)
 
