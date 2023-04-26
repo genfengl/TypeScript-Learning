3cf67100244e089c49c57e423c04c50042b0a099
genfengl <genfeng.liu@gmail.com>
2023-04-26 13:32:22 +1000

    generic type constraints, keyof, utility type

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -40,3 +40,61 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 console.log(extractAndConvert({ name: 'Max' }, 'name'));
+
+// * better to use T extends string | number | boolean
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = []
+
+  addItem(item: T) {
+    this.data.push(item)
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return
+    }
+    this.data.splice(this.data.indexOf(item), 1)
+  }
+
+  getItems() {
+    return [...this.data]
+  }
+}
+
+const textStorage = new DataStorage<string>()
+textStorage.addItem('Max')
+textStorage.addItem('Manu')
+textStorage.removeItem('Max')
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>()
+
+// const objStorage = new DataStorage<object>()
+// const maxObj = { name: 'Max' }
+
+// objStorage.addItem({ name: 'Max' })
+// objStorage.addItem({ name: 'Manu' })
+// objStorage.removeItem(maxObj)
+// console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string
+  description: string
+  completeUntil: Date
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  // * an object that in the end will become CourseGoal, making keys in an object be optional temporarily 
+  let courseGoal: Partial<CourseGoal> = {}
+  courseGoal.title = title
+  courseGoal.description = description
+  courseGoal.completeUntil = date
+  return courseGoal as CourseGoal
+}
+
+const newNames: Readonly<string[]> = ['Max', 'Anna']
+// newNames.push('Manu')
