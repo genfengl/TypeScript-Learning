d5f588dbfd8bfd0847d740c0abde18e1589f1fb9
genfengl <genfeng.liu@gmail.com>
2023-04-27 17:02:34 +1000

    node trial

--- node-01-starting-setup/src/app.ts
+++ node-01-starting-setup/src/app.ts
@@ -1,5 +1,21 @@
-const express = require('express')
+// use npm i --save-dev @types/node and npm i --save-dev @types/express to add type support (autocompletion)
 
-const app = express()
+// experimental syntax in JS, but standard in TS
 
-app.listen(3000)
+import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
+
+import todoRoutes from "./routes/todos";
+
+const app = express();
+
+app.use(json())
+
+app.use("/todos", todoRoutes);
+
+// imported from line 5
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
+
+app.listen(3000);
---
+++ node-01-starting-setup/src/controllers/todos.ts
@@ -0,0 +1,5 @@
+import { RequestHandler } from "express"
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  
+}
