36c5c6c8e0f84613a8c2e39fc83f27db92b2b086
genfengl <genfeng.liu@gmail.com>
2023-06-07 08:31:13 +1000

    added notes to cheat sheet

--- typescript-cheat-sheets/src/index.ts
+++ typescript-cheat-sheets/src/index.ts
@@ -1,5 +1,8 @@
 // Common typescript uses
 
+// config: strictness
+// noImplicity and strickNullChecks?
+
 // Object
 const user: User = {
   name: "Gerald",
@@ -12,21 +15,40 @@ interface User {
 }
 
 class UserAccount {
-  name: string
-  id: number
+  name: string;
+  id: number;
 
   constructor(name: string, id: number) {
-    this.name = name
-    this.id = id
+    this.name = name;
+    this.id = id;
   }
 }
 
 //? Is this the same as class UserAccount implements User?
-const newUser: User = new UserAccount("Gerald", 1)
+const newUser: User = new UserAccount("Gerald", 1);
 
 // Unions
-type MyBool = true | false
+type MyBool = true | false;
 
 // Literals
-type WindowStates = "open" | "closed" | "minimised"
-type OddNumberUnderTen = 1|3|5|7|9
+type WindowStates = "open" | "closed" | "minimised";
+type OddNumberUnderTen = 1 | 3 | 5 | 7 | 9;
+
+// Can be string or an array of strings
+const obj: (string | string[]) = []
+
+// Narrowing union types
+if (Array.isArray(obj)) {
+  console.log("obj is an array")
+} else {
+  console.log(obj)
+}
+
+// use "?" for optional or maybe null, use "!" for non-null assertion
+
+
+
+
+// Generics
+// Need to dig deepter
+
