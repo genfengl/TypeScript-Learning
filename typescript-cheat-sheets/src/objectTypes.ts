ccbe8c7fd5deae0f6e177faac7c4f80278ad7234
genfengl <genfeng.liu@gmail.com>
2023-06-08 15:13:30 +1000

    extending types

---
+++ typescript-cheat-sheets/src/objectTypes.ts
@@ -0,0 +1,36 @@
+// readonly Properties
+// readonly properties can also change via aliasing.
+interface Person {
+  name: string;
+  age: number;
+}
+ 
+interface ReadonlyPerson {
+  readonly name: string;
+  readonly age: number;
+}
+ 
+let writablePerson: Person = {
+  name: "Person McPersonface",
+  age: 42,
+};
+ 
+// works
+let readonlyPerson: ReadonlyPerson = writablePerson;
+ 
+console.log(readonlyPerson.age); // prints '42'
+writablePerson.age++;
+console.log(readonlyPerson.age); // prints '43'
+
+// Extending Types
+interface BasicAddress {
+  name?: string;
+  street: string;
+  city: string;
+  country: string;
+  postalCode: string;
+}
+ 
+interface AddressWithUnit extends BasicAddress {
+  unit: string;
+}
