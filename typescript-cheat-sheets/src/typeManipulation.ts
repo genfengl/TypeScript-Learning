531fef84d9c93f4fee60d2837fbb71f1f19fa442
genfengl <genfeng.liu@gmail.com>
2023-06-08 17:18:39 +1000

    conditional types

--- typescript-cheat-sheets/src/typeManipulation.ts
+++ typescript-cheat-sheets/src/typeManipulation.ts
@@ -6,7 +6,6 @@ type P = keyof Point;
 let s = "hello";
 let n: typeof s;
 
-
 function f() {
   return { x: 10, y: 3 };
 }
@@ -22,10 +21,10 @@ type Age = Person1["age"];
 
 type I1 = Person1["age" | "name"];
 // type I1 = string | number
- 
+
 type I2 = Person1[keyof Person1];
 // type I2 = string | number | boolean
- 
+
 type AliveOrName = "alive" | "name";
 type I3 = Person1[AliveOrName];
 // type I3 = string | boolean
@@ -35,14 +34,42 @@ const MyArray = [
   { name: "Bob", age: 23 },
   { name: "Eve", age: 38 },
 ];
-type Person2 = typeof MyArray[number];
+type Person2 = (typeof MyArray)[number];
 // type Person2 = {
 //   name: string;
 //   age: number;
 // }
-type Age2 = typeof MyArray[number]["age"]
+type Age2 = (typeof MyArray)[number]["age"];
 // type Age2 = number
 
 //* Or
 //* type Age2 = Person2["age"]
 
+// Conditional Types
+interface Animal {
+  live(): void;
+}
+interface Dog extends Animal {
+  woof(): void;
+}
+
+type Example1 = Dog extends Animal ? number : string;
+// type Example1 = number
+
+type Example2 = RegExp extends Animal ? number : string;
+// type Example2 = string
+
+interface IdLabel {
+  id: number /* Defines the id property as a number */;
+}
+
+interface NameLabel {
+  name: string /* Defines the name property as a string */;
+}
+
+// The following type declaration defines a conditional type called NameOrId.
+// It takes a type parameter T that extends either number or string.
+
+type NameOrId<T extends number | string> = T extends number
+  ? IdLabel // If T extends number, the type NameOrId returns IdLabel interface.
+  : NameLabel; // If T extends string, the type NameOrId returns NameLabel interface.
