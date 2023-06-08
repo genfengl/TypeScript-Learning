55d1309229b91268f2bad3a567816440ec784218
genfengl <genfeng.liu@gmail.com>
2023-06-08 15:16:57 +1000

    intersection types

--- typescript-cheat-sheets/src/objectTypes.ts
+++ typescript-cheat-sheets/src/objectTypes.ts
@@ -4,25 +4,25 @@ interface Person {
   name: string;
   age: number;
 }
- 
+
 interface ReadonlyPerson {
   readonly name: string;
   readonly age: number;
 }
- 
+
 let writablePerson: Person = {
   name: "Person McPersonface",
   age: 42,
 };
- 
+
 // works
 let readonlyPerson: ReadonlyPerson = writablePerson;
- 
+
 console.log(readonlyPerson.age); // prints '42'
 writablePerson.age++;
 console.log(readonlyPerson.age); // prints '43'
 
-// Extending Types
+// Extending Types (use "extend")
 interface BasicAddress {
   name?: string;
   street: string;
@@ -30,7 +30,17 @@ interface BasicAddress {
   country: string;
   postalCode: string;
 }
- 
+
 interface AddressWithUnit extends BasicAddress {
   unit: string;
 }
+
+// Intersection Types (use "&")
+interface Colorful {
+  color: string;
+}
+interface Circle {
+  radius: number;
+}
+
+type ColorfulCircle = Colorful & Circle;
