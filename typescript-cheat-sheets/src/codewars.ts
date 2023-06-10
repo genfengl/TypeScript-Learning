ca397ccab76730f36ca3dfce41f7f460a1edccbc
genfengl <genfeng.liu@gmail.com>
2023-06-10 17:53:33 +1000

    added a 4 kyu question

--- typescript-cheat-sheets/src/codewars.ts
+++ typescript-cheat-sheets/src/codewars.ts
@@ -24,9 +24,109 @@ function buddy(start: number, limit: number): number[] {
     const sumOfM = getSumOfDivisors(sumOfN - 1);
     if (sumOfM === i + 1 && sumOfN - 1 > i) {
       // Found a buddy pair, return it as an array
-      return output = [i, sumOfN - 1];
+      return (output = [i, sumOfN - 1]);
     }
   }
   // No buddy pairs found, return an empty array
   return output;
+}
+
+function isInteresting(n: number, awesomePhrases: number[]): number {
+  // CODE HERE
+  console.log("n:", n, "phrases:", awesomePhrases);
+  if (awesomePhrases) {
+    if (awesomePhrases[0] === n || awesomePhrases[1] === n) {
+      return 2;
+    }
+    if (
+      n + 2 === awesomePhrases[0] ||
+      n + 2 === awesomePhrases[1] ||
+      n + 1 === awesomePhrases[0] ||
+      n + 1 === awesomePhrases[1]
+    ) {
+      return 1;
+    }
+  }
+  if (n < 10) {
+    return 0;
+  }
+
+  for (let i = 0; i <= 2; i++) {
+    if (
+      isAllZeroes(n + i) ||
+      isSameNumber(n + i) ||
+      isIncrementing(n + i) ||
+      isDecrementing(n + i) ||
+      isPalindrome(n + i)
+    ) {
+      if (i === 0) {
+        return 2;
+      } else if (i === 1 || i === 2) {
+        return 1;
+      }
+    }
+  }
+
+  function isAllZeroes(n: number): boolean {
+    if (n < 100) {
+      return false;
+    }
+    if (n.toString().match(/^\d0*$/)) {
+      return true;
+    }
+    return false;
+  }
+  function isSameNumber(n: number): boolean {
+    //     if (n.toString().match(/^(\d)\1*$/)) {
+    //       return true
+    //     }
+    if (n < 100) {
+      return false;
+    }
+    return new Set([...n.toString()]).size === 1;
+  }
+  function isIncrementing(n: number): boolean {
+    //     return `01234567890`.includes(n.toString())
+    if (n < 100) {
+      return false;
+    }
+    const nStr = n.toString();
+    for (let i = 0; i < nStr.length - 1; i++) {
+      if (i === nStr.length - 2) {
+        if (nStr[i] === "9") {
+          return nStr[i + 1] === "0";
+        }
+      }
+      if (Number(nStr[i + 1]) - Number(nStr[i]) !== 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function isDecrementing(n: number): boolean {
+    //     return `9876543210`.includes(n.toString())
+    if (n < 100) {
+      return false;
+    }
+    const nStr = n.toString();
+    for (let i = 0; i < nStr.length - 1; i++) {
+      if (Number(nStr[i]) - Number(nStr[i + 1]) !== 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  function isPalindrome(n: number): boolean {
+    if (n < 100) {
+      return false;
+    }
+    const nStr = n.toString();
+
+    if (nStr.split("").reverse().join("") === nStr) {
+      return true;
+    }
+    return false;
+  }
+
+  return 0;
 }
